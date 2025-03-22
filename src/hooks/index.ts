2c6539524a62969b50a28addc4a6b153ec8debe8
elliotreed <elliot@elliotreed.net>
2025-03-22 13:19:27 -0600

    modified export structure for directory

---
+++ src/hooks/index.ts
@@ -0,0 +1,9 @@
+import { useKeyPress } from '@/hooks/useKeyPress';
+import { useOutsideAlerter } from './useOutsideAlerter';
+import { useSimulateButtonPress } from './useSimulateButtonPress';
+
+export {
+    useKeyPress,
+    useOutsideAlerter,
+    useSimulateButtonPress,
+}
---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { useAudioStore } from './useAudioStore'; './useAudioStore';
+import { useMetronomeStore } from './useMetronomeStore';
+import { useTempoTrainerStore } from './useTempoTrainerStore';
+
+export {
+    useAudioStore,
+    useMetronomeStore,
+    useTempoTrainerStore
+}
