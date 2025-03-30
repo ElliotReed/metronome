01da32731e4d703f42dfb355026a555390aacb8b
elliotreed <elliot@elliotreed.net>
2025-03-29 18:42:30 -0600

    export useTimer

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,11 @@
-import { useKeyPress } from '@/hooks/useKeyPress';
+import { useKeyPress } from './useKeyPress';
 import { useOutsideAlerter } from './useOutsideAlerter';
 import { useSimulateButtonEvents } from './useSimulateButtonEvents';
+import { useTimer } from './useTimer';
 
 export {
     useKeyPress,
     useOutsideAlerter,
     useSimulateButtonEvents,
+    useTimer,
 }
