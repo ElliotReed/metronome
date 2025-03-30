264e4f6e9712d7442d80b730e14db24096343382
elliotreed <elliot@elliotreed.net>
2025-03-29 18:43:22 -0600

    Added beat info

--- src/store/useMetronomeStore.ts
+++ src/store/useMetronomeStore.ts
@@ -8,17 +8,28 @@ export type MetronomeSettings = {
     decrementBpm: () => void;
     beatsPerMeasure: number;
     setBeatsPerMeasure: (beats: number) => void;
+    beatValue: number;
+    setBeatValue: (value: number) => void;
+    beatCount: number;
+    incrementBeatCount: () => void;
+    resetBeatCount: () => void;
 }
 
-export const useMetronomeStore = create<MetronomeSettings>()(persist(
-    (set) => ({
-        bpm: 120,
-        setBpm: (bpm: number) => set({ bpm }),
-        incrementBpm: () => set((state) => ({ bpm: state.bpm + 1 })),
-        decrementBpm: () => set((state) => ({ bpm: state.bpm - 1 })),
-        beatsPerMeasure: 4,
-        setBeatsPerMeasure: (beats: number) => set({ beatsPerMeasure: beats }),
-        beatValue: 4,
-    }),
-    { name: 'cm-metronome-settings' }
-));
+export const useMetronomeStore = create<MetronomeSettings>()(
+    persist(
+        (set) => ({
+            bpm: 120,
+            setBpm: (bpm: number) => set({ bpm }),
+            incrementBpm: () => set((state) => ({ bpm: state.bpm + 1 })),
+            decrementBpm: () => set((state) => ({ bpm: state.bpm - 1 })),
+            beatsPerMeasure: 4,
+            setBeatsPerMeasure: (beats: number) => set({ beatsPerMeasure: beats }),
+            beatValue: 4,
+            setBeatValue: (value: number) => set({ beatValue: value }),
+            beatCount: 0,
+            incrementBeatCount: () => set((state) => ({ beatCount: state.beatCount < state.beatsPerMeasure ? state.beatCount + 1 : 1 })),
+            resetBeatCount: () => set({ beatCount: 0 })
+        }),
+        { name: 'cm-metronome-settings' }
+    )
+);
