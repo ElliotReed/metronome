f72a7a8e589733ec7be8628b325fea3ce5dabe53
elliotreed <elliot@elliotreed.net>
2025-03-31 11:33:54 -0600

    Deleted TimerEngine

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,7 @@
-import { timerInstance } from '@/utils/timerEngine';
 import { metronomeStops } from '@/utils/metronomeStops';
 import { getIntervalFromBpm } from '@/utils/getBpmFromInterval';
 
 export {
     getIntervalFromBpm,
     metronomeStops,
-    timerInstance as TimerEngine
-
 }
